4d66d00eae498185741781bce44acc254f2bbc19
Bogdan <bogdan30-95@mail.ru>
2025-10-30 10:48:11 +0300

    added TextBoxTests

---
+++ src/test/java/tests/TextBoxTests.java
@@ -0,0 +1,37 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TextBoxTests {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
+
+
+    }
+
+    @Test
+    void fillFormTest() {
+        open("/text-box");
+        $("#userName").setValue("Alex");
+        $("#userEmail").setValue("alex@egorov.com");
+        $("#currentAddress").setValue("Street Main");
+        $("#permanentAddress").setValue("Double Street Main");
+        $("#submit").scrollTo().click();
+
+        $("#output #name").shouldHave(text("Alex"));
+        $("#output #email").shouldHave(text("alex@egorov.com"));
+        $("#output #currentAddress").shouldHave(text("Street Main"));
+        $("#output #permanentAddress").shouldHave(text("Double Street Main"));
+
+    }
+}
