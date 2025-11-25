44db29c968bf6610916ff091a3dd951576ac69e1
Bogdan <bogdan30-95@mail.ru>
2025-11-25 22:08:32 +0300

    added remote demoqa test

--- src/test/java/pages/TestConfiguration.java
+++ src/test/java/pages/TestConfiguration.java
@@ -12,7 +12,7 @@ public class TestConfiguration {
         Configuration.browser = "chrome";
         Configuration.timeout = 3000;
         Configuration.pageLoadStrategy = "eager";
-        Configuration.holdBrowserOpen = true;
+        Configuration.holdBrowserOpen = false;
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 }
