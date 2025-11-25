d61ddcd739b77a81dea5e801d42a08fa6b36fba8
Bogdan <bogdan30-95@mail.ru>
2025-11-25 20:36:50 +0300

    added allure steps  RegistrationNotCompleteTests.java

--- src/test/java/tests/RegistrationNotCompleteTests.java
+++ src/test/java/tests/RegistrationNotCompleteTests.java
@@ -1,58 +1,80 @@
 package tests;
 
+import io.qameta.allure.Link;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import pages.TestConfiguration;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
 
-
-
+@Owner("Chufarov Bogdan")
+@Severity(SeverityLevel.NORMAL)
 class RegistrationNotCompleteTests extends TestConfiguration {
 
+
     @Test
+    @Link(name = "Testing", url = "https://demoqa.com")
+    @DisplayName("Форма регистрации на сайте")
+    @Tag("demoqa")
     void checkFormTest() {
-        open("/automation-practice-form");
 
-        $("#firstName").setValue("Alex");
-        $("#lastName").setValue("Ruby");
-        $("#userEmail").setValue("alex@gmail.com");
-        $("#genterWrapper").$(byText("Male")).click();
-        //$("[for=gender-radio-1]").click();
-        //$("#gender-radio-1").parent().click();
+        step("Open registration form", () -> {
+            open("/automation-practice-form");
+        });
 
-        $("#userNumber").setValue("1234567898");
+        step("Fill form and press the button Submit", () -> {
 
-        $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").selectOption("June");
-        //$(".react-datepicker__month-select").selectOptionByValue("5");
-        $(".react-datepicker__year-select").selectOption("1990");
-        //$(".react-datepicker__year-select").selectOptionByValue("1990");
-        $(".react-datepicker__day--025").click();
+            $("#firstName").setValue("Alex");
+            $("#lastName").setValue("Ruby");
+            $("#userEmail").setValue("alex@gmail.com");
+            $("#genterWrapper").$(byText("Male")).click();
+            //$("[for=gender-radio-1]").click();
+            //$("#gender-radio-1").parent().click();
+            $("#userNumber").setValue("1234567898");
+            $("#dateOfBirthInput").click();
+            $(".react-datepicker__month-select").selectOption("June");
+            //$(".react-datepicker__month-select").selectOptionByValue("5");
+            $(".react-datepicker__year-select").selectOption("1990");
+            //$(".react-datepicker__year-select").selectOptionByValue("1990");
+            $(".react-datepicker__day--025").click();
+            $("#subjectsInput").setValue("Arts").pressEnter();
+            $("#hobbiesWrapper").$(byText("Sports")).click();
+            $("#uploadPicture").uploadFromClasspath("Images.jfif");
+            $("#currentAddress").setValue("Street Main").scrollTo();
+            $("#state").click();
+            $(byText("NCR")).click();
+            $("#city").click();
+            $(byText("Delhi")).click();
+            $("#submit").click();
+        });
 
-        $("#subjectsInput").setValue("Arts").pressEnter();
-        $("#hobbiesWrapper").$(byText("Sports")).click();
-        $("#uploadPicture").uploadFromClasspath("Images.jfif");
-        $("#currentAddress").setValue("Street Main").scrollTo();
-        $("#state").click();
-        $(byText("NCR")).click();
-        $("#city").click();
-        $(byText("Delhi")).click();
-        $("#submit").click();
+        step("Verify results registration", () -> {
+            $(".modal-content").shouldBe(visible);
+            $(".table-responsive").shouldBe(visible);
+            $(".table-responsive").shouldHave(text("Alex"), text("Ruby"), text("alex@gmail.com"));
+            $(".table-responsive").shouldHave(text("Male"));
+            $(".table-responsive").shouldHave(text("1234567898"));
+            $(".table-responsive").shouldHave(text("25 June,1990"));
+            $(".table-responsive").shouldHave(text("Arts"));
+            $(".table-responsive").shouldHave(text("Sports"));
+            $(".table-responsive").shouldHave(text("Images.jfif"));
+            $(".table-responsive").shouldHave(text("Street Main"));
+            $(".table-responsive").shouldHave(text("NCR Delhi"));
+        });
 
-        $(".modal-content").shouldBe(visible);
-        $(".table-responsive").shouldBe(visible);
 
-        $(".table-responsive").shouldHave(text("Alex"), text("Ruby"), text("alex@gmail.com"));
-        $(".table-responsive").shouldHave(text("Male"));
-        $(".table-responsive").shouldHave(text("1234567898"));
-        $(".table-responsive").shouldHave(text("25 June,1990"));
-        $(".table-responsive").shouldHave(text("Arts"));
-        $(".table-responsive").shouldHave(text("Sports"));
-        $(".table-responsive").shouldHave(text("Images.jfif"));
-        $(".table-responsive").shouldHave(text("Street Main"));
-        $(".table-responsive").shouldHave(text("NCR Delhi"));
+
+
+
+
 
 
     }
